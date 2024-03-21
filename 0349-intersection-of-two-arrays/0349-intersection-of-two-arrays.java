323279049673be84455c88caafa8fbd434315de2
Vijayendra Sai Ch <62643294+VijayendraSai@users.noreply.github.com>
2024-03-21 16:53:23 -0400

    Time: 0 ms (100%), Space: 42.9 MB (85.8%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,48 +1,25 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        insertion(nums1);
-        insertion(nums2);
-        Set<Integer> res = new HashSet<Integer>();
-        int i=0, j=0;
-        while(i<nums1.length && j<nums2.length) {
-            if(nums1[i] == nums2[j]) {
-                res.add(nums1[i]);
-                i++;
+        int[] temp = new int[1001];
+        for (int i : nums1) {
+            temp[i] = 1;
+        }
+        int count = 0;
+        for (int i : nums2) {
+            if (temp[i] == 1) {
+                count++;
+                temp[i] = 2;
+            }
+        }
+        int arr[] = new int[count];
+        int j = 0;
+        for (int i : nums2) {
+            if (temp[i] == 2) {
+                temp[i] = 1;
+                arr[j] = i;
                 j++;
             }
-            else if(nums1[i] > nums2[j]) {
-                j++;
-            }
-            else {
-                i++;
-            }
         }
-        // Convert set to int array
-        int[] result = new int[res.size()];
-        int index = 0;
-        for (int element : res) {
-            result[index++] = element;
-        }
-
-        return result;
-    }
-
-    static void insertion(int[] arr) {
-        for(int i=0; i<arr.length-1; i++) {
-            for(int j=i+1; j>0; j--) {
-                if(arr[j-1] > arr[j]) {
-                    swap(arr, j-1, j);
-                } 
-                else {
-                    break;
-                }
-            }
-        }
-    }
-
-    static void swap(int[] arr, int first, int second) {
-        int temp = arr[first];
-        arr[first] = arr[second];
-        arr[second] = temp;
+        return arr;
     }
 }
