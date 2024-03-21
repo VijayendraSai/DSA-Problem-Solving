a67707c0a6449c626a5952f3204c9e92cd65b79f
Vijayendra Sai Ch <62643294+VijayendraSai@users.noreply.github.com>
2024-03-21 13:17:05 -0400

    Time: 1 ms (99.66%), Space: 52.9 MB (17.01%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,43 +1,18 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        insertion(nums);
-
-        int maxVal=nums[0], currVal=nums[0], count = 1;
-        for(int i=1; i<nums.length; i++) {
-            if(nums[i] == currVal) {
-                ++count;
+        int maj = nums[0];
+        int count = 1;
+        for(int i = 1; i< nums.length; i++){
+            if(nums[i] == maj)
+                count++;
+            else{
+                count--;
             }
-            else {
-                if(count > Math.floor(nums.length/2)) {
-                    maxVal = currVal;
+            if(count == 0){
+                maj = nums[i];
+                count = 1;
                 }
-                currVal = nums[i];
-                count = 1; 
-            }
         }
-
-        if(count > Math.floor(nums.length/2)) {
-            maxVal = currVal;
-        }
-        return maxVal;
-    }
-
-    static void insertion(int[] arr) {
-        for(int i=0; i<arr.length-1; i++) {
-            for(int j=i+1; j>0; j--) {
-                if(arr[j-1] > arr[j]) {
-                    swap(arr, j, j-1);
-                }
-                else {
-                    break;
-                }
-            }
-        }
-    }
-
-    static void swap(int[] arr, int p, int q) {
-        int val = arr[p];
-        arr[p] = arr[q];
-        arr[q] = val; 
+        return maj;
     }
 }
