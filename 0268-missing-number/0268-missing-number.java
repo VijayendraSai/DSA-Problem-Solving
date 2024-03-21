bc5ae30fa27f21ad9dfe25e0a201e9128bcc1b79
Vijayendra Sai Ch <62643294+VijayendraSai@users.noreply.github.com>
2024-03-21 15:55:17 -0400

    Time: 0 ms (100%), Space: 45.4 MB (20.06%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,31 +1,13 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        cyclic_sort(nums);
+        int sumOfArray = 0;
+        int sumOfRange = nums.length;
 
-        for(int i=0; i<nums.length; i++) {
-            if(nums[i] != i) {
-                return i;
-            }
+        for (int i = 0; i < nums.length; i++) {
+            sumOfRange += i;
+            sumOfArray += nums[i];
         }
-        return nums.length;
-    }
 
-    static void cyclic_sort(int[] arr) {
-        int i = 0;
-        while(i < arr.length) {
-            int correct = arr[i];
-            if(correct < arr.length && arr[i] != arr[correct]) {
-                swap(arr, i, correct);
-            }
-            else {
-                i++;
-            }
-        }
-    }
-
-    static void swap(int[] arr, int first, int second) {
-        int temp = arr[first];
-        arr[first] = arr[second];
-        arr[second] = temp;
+        return sumOfRange - sumOfArray;
     }
 }
