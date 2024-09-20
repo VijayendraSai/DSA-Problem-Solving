34d339c179dafe4baa97d798e060d3e9e79560cd
Vijayendra Sai Ch <62643294+VijayendraSai@users.noreply.github.com>
2024-09-20 14:59:26 -0400

    Time: 2 ms (8.44%), Space: 45.3 MB (49.1%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.java
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.java
@@ -1,9 +1,24 @@
 class Solution {
     public int[] buildArray(int[] nums) {
-        int[] ans = new int[nums.length];
+        int q = nums.length, b=0, r=0;
+
+        // turn the array into a=qb+r
         for(int i=0; i<nums.length; i++) {
-            ans[i] = nums[nums[i]];
+            r = nums[i];
+            
+            /* retrieve correct value from potentially already processed element
+	            i.e. get r from something maybe already in the form a = qb + r
+	            if it isnt already processed (doesnt have qb yet), that's ok b/c
+	            r < q, so r % q will return the same value */
+            b = nums[nums[i]] % q;
+
+            // put it all together
+            nums[i] = q*b + r;
         }
-        return ans;
+        // extract just the final b values
+        for(int i=0; i<nums.length; i++) {
+            nums[i] = nums[i] / q;
+        }
+        return nums;
     }
 }
