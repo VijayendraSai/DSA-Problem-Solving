54eb0175eefbb091f0b2831fc04f7022713daaca
Vijayendra Sai Ch <62643294+VijayendraSai@users.noreply.github.com>
2024-10-04 15:26:19 -0400

    Time: 2 ms (98.76%), Space: 46.2 MB (8.15%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
@@ -1,13 +1,13 @@
 class Solution {
     public List<Integer> addToArrayForm(int[] num, int k) {
-        LinkedList<Integer> res=new LinkedList<>();
-        int len=num.length-1;
-        while(len>=0 || k>0){
-            if(len>=0){
-                k+=num[len--];
-            }
-            res.addFirst(k%10);
-            k/=10;
+        List<Integer> res = new LinkedList<>();
+        for (int i = num.length - 1; i >= 0; --i) {
+            res.add(0, (num[i] + k) % 10);
+            k = (num[i] + k) / 10;
+        }
+        while (k > 0) {
+            res.add(0, k % 10);
+            k /= 10;
         }
         return res;
     }
