e95d2c078dc07c96d658720ecd70880b039e78b9
Vijayendra Sai Chennareddy <62643294+VijayendraSai@users.noreply.github.com>
2025-01-22 18:04:09 -0500

    Time: 0 ms (100.00%) | Memory: 41.1 MB (97.30%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,28 +1,14 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if(strs.length == 1) return strs[0];
-        String prefix = findCommonPrefix(strs[0], strs[1]);
-        if(prefix.equals("")) return "";
-
-        for(int i=2; i<strs.length; i++) {
-            prefix = findCommonPrefix(prefix, strs[i]);
-            if(prefix.equals("")) return "";
-        }
-        return prefix;
-
-    }
-    public static String findCommonPrefix(String str1, String str2) {
-        int minLength = Math.min(str1.length(), str2.length());
-        String prefix = new String("");
-
-        for (int i = 0; i < minLength; i++) {
-            if (str1.charAt(i) != str2.charAt(i)) {
-                return prefix;
-            }
-            else {
-                prefix = prefix + "" + str1.charAt(i);
+        if (strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if(prefix.isEmpty()) return "";
             }
         }
         return prefix;
+
     }
 }
