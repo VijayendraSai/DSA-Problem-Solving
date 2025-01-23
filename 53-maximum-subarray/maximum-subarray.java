6107b41801ae3d0cd36ea2dbfddd46023d9086d7
Vijayendra Sai Chennareddy <62643294+VijayendraSai@users.noreply.github.com>
2025-01-22 22:55:46 -0500

    Time: 1 ms (99.37%) | Memory: 57.4 MB (20.98%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,45 +1,15 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        return findMaxSubarray(nums, 0, nums.length - 1);
-    }
-
-    private int findMaxSubarray(int[] nums, int left, int right) {
-        // Base case: only one element
-        if (left == right) {
-            return nums[left];
-        }
-
-        // Find the midpoint
-        int mid = left + (right - left) / 2;
-
-        // Recursively find the max subarray in the left and right halves
-        int leftMax = findMaxSubarray(nums, left, mid);
-        int rightMax = findMaxSubarray(nums, mid + 1, right);
-
-        // Find the max subarray that crosses the midpoint
-        int crossMax = findCrossMax(nums, left, mid, right);
-
-        // Return the maximum of the three
-        return Math.max(Math.max(leftMax, rightMax), crossMax);
-    }
-
-    private int findCrossMax(int[] nums, int left, int mid, int right) {
-        // Find max sum in the left half, ending at mid
-        int leftSum = Integer.MIN_VALUE, sum = 0;
-        for (int i = mid; i >= left; i--) {
+        int n = nums.length;
+        int max = Integer.MIN_VALUE, sum = 0;
+        
+        for(int i=0;i<n;i++){
             sum += nums[i];
-            leftSum = Math.max(leftSum, sum);
+            max = Math.max(sum,max);
+            
+            if(sum<0) sum = 0;
         }
-
-        // Find max sum in the right half, starting at mid+1
-        int rightSum = Integer.MIN_VALUE;
-        sum = 0;
-        for (int i = mid + 1; i <= right; i++) {
-            sum += nums[i];
-            rightSum = Math.max(rightSum, sum);
-        }
-
-        // Combine the two
-        return leftSum + rightSum;
+        
+        return max;
     }
 }
